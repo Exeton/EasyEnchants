77adc92222bd18758f74b64b34432d1ff00a9b78
Exeton <benjamin.diederich@gmail.com>
2019-03-04 18:41:03 -0500

    Added a random enchant

---
+++ easy_enchants/src/main/java/online/fireflower/easy_enchants/enchant_types/RandomEnchant.java
@@ -0,0 +1,23 @@
+package online.fireflower.easy_enchants.enchant_types;
+
+import online.fireflower.easy_enchants.enchant_parsing.EnchantInfo;
+import org.bukkit.event.Event;
+
+import java.util.Random;
+
+public abstract class RandomEnchant extends Enchant {
+
+    private Random random;
+
+    public RandomEnchant(String displayName, Random random) {
+        super(displayName);
+        this.random = random;
+    }
+
+    @Override
+    public boolean shouldActivate(Event event, EnchantInfo enchantInfo) {
+        return random.nextInt(100) <= calculatePercentActivation(event, enchantInfo);
+    }
+
+    public abstract int calculatePercentActivation(Event event, EnchantInfo enchantInfo);
+}
